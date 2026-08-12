fd32ae5551313bc90668363a1489629a72e4651b
Paresh-Gowda <pareshgowda16@gmail.com>
2026-08-13 00:28:26 +0530

    Completed Session 16 and Session 17 - Dice game project

---
+++ JS/Manipulation/script.js
@@ -0,0 +1,5 @@
+document.querySelector("button").style.backgroundColor="yellow";
+document.querySelector("li a").textContent="Spam Link";
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").innerHTML="Hi!"
+document.querySelector("li a").setAttribute("href", "https://github.in");
