c39f2dff3d279b1b4dca4c4d2a7d66b2fd31b9d5
Paresh-Gowda <pareshgowda16@gmail.com>
2026-08-16 02:58:55 +0530

    Completed Session 24

---
+++ Node.js/HTTP Requests/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app=express();
+const port=3000;
+app.get("/", (req, res) => {
+    res.send("<h1>Hello</h1>");
+});
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me</h1><p>My name is Paresh</p>");
+});
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Me</h1><p>Tele: +12345</p>");
+});
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
---
+++ Node.js/Mini Projects/Secret Access/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname=dirname(fileURLToPath(import.meta.url));
+const app=express();
+const port=3000;
+var userAuthorised=false;
+app.use(bodyParser.urlencoded({extended: true}));
+function passwordCheck(req,res,next) {
+    const password=req.body["password"];
+    if(password==="ILoveProgramming") {
+        userAuthorised=true;
+    }
+    next();
+}
+app.use(passwordCheck);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+app.post("/check", (req, res) => {
+    if(passwordCheck) {
+        res.sendFile(__dirname + "/public/secret.html");
+    }
+    else {
+        res.redirect("/");
+    }
+})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
