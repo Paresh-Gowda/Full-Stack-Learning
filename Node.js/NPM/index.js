8e1d2071a65816839305818f01424e9775cab54e
Paresh-Gowda <pareshgowda16@gmail.com>
2026-08-15 21:34:07 +0530

    Started learning Backend & Completed Session 23 with QR Code Generator Project

---
+++ Node.js/NPM/index.js
@@ -0,0 +1,6 @@
+import generateName from "sillyname";
+var sillyName=generateName();
+console.log(`My name is ${sillyName}.`)
+import { randomSuperhero } from "superheroes";
+const name=randomSuperhero();
+console.log(`I am ${name}.`);
