24ee260b3b1883e148cae86cd5ec1620edbe3542
Paresh-Gowda <pareshgowda16@gmail.com>
2026-08-21 02:16:30 +0530

    Completed Part-2 of React Module

---
+++ React/Mapping Data/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Emoji from "./Emoji";
+import emojipedia from "../emojipedia";
+function createEntry(emojiTerm) {
+  return (
+    <Emoji
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+export default App;
---
+++ React/Mapping Data/src/emojipedia.js
@@ -0,0 +1,31 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "I love this! A smiling face with hearts instead of eyes, showing love, admiration, or excitement. Often used when something or someone looks absolutely amazing.",
+  },
+];
+export default emojipedia;
